ce5e5c461750b8b6c866114a9b921d39d67db623
James Reed <jamesr66@vt.edu>
2014-05-05 14:00:37 -0400

    Added actual connection for C++ interface

--- weechat_cpp/WeeChatConnection.py
+++ weechat_cpp/WeeChatConnection.py
@@ -1,23 +1,20 @@
 import ctypes
 
 class WeeChatConnection:
-    def __init__(self):
+    def __init__(self, server, port, ipv6, ssl):
         self.library = ctypes.CDLL('./WeeChatConnection.so')
-        self.class_ptr = self.library.WeeChatConnection_constr()
+        self.class_ptr = self.library.WeeChatConnection_constr(server, port, ipv6, ssl)
     
-    def init(self, server, port, ipv6, ssl):
-        self.library.WeeChatConnection_init(self.class_ptr, server, port, ipv6, ssl)
+    def init(self, password):
+        self.library.WeeChatConnection_init(self.class_ptr, password)
     
     def SendMessage(self, message):
-        str_p = ctypes.c_char_p(self.library.WeeChatConnection_send_msg(self.class_ptr, message))
-        str = str_p.value
-        self.library.free_buf(str_p)
-        return str
+        self.library.WeeChatConnection_send_msg(self.class_ptr, message)
     
     def __del__(self):
         self.library.WeeChatConnection_destroy(self.class_ptr)
 
 if __name__ == '__main__':
-    wcc = WeeChatConnection()
-    wcc.init("www.google.com", 21, False, True)
-    print(wcc.SendMessage("Message!"))
+    wcc = WeeChatConnection('128.173.88.125', 9000, False, False)
+    wcc.init("reeelay")
+    wcc.SendMessage("info version")
--- weechat_cpp/include/WeeChatConnection.h
+++ weechat_cpp/include/WeeChatConnection.h
@@ -3,38 +3,81 @@
 
 #include <string>
 #include <cstring>
+#include <exception>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+
+class SocketException : public std::exception
+{
+private:
+    std::string what_string;
+public:
+    SocketException(std::string what_string)
+    {
+        this->what_string = what_string;
+    }
+    
+    virtual const char* what() const throw()
+    {
+        return what_string.c_str();
+    }
+    ~SocketException() throw() {}
+};
+
+class CommunicationException : public std::exception
+{
+private:
+    std::string what_string;
+public:
+    CommunicationException(std::string what_string)
+    {
+        this->what_string = what_string;
+    }
+    virtual const char* what() const throw()
+    {
+        return what_string.c_str();
+    }
+    ~CommunicationException() throw() {}
+};
 
 class WeeChatConnection
 {
+private:
+    int sockfd;
+    sockaddr_in serv_addr;
+    hostent *server;
+    
 public:
-    WeeChatConnection();
-    void init(std::string server, int port, bool ipv6, bool ssl);
-    std::string send_msg(std::string message);
+    WeeChatConnection(std::string server, int port, bool ipv5, bool ssl);
+
+    void init(std::string password);
+    void send_msg(std::string message);
 };
 
 //python wrappings
 extern "C"
 {
-    WeeChatConnection *WeeChatConnection_constr()
+    void WeeChatConnection_init(WeeChatConnection *self, char* password)
     {
-        return new WeeChatConnection();
+        self->init(password);
     }
 
-    void WeeChatConnection_init(WeeChatConnection* self,
-                                char* server,
-                                int port,
-                                bool ipv6,
-                                bool ssl)
+    WeeChatConnection *WeeChatConnection_constr(char* server,
+                                                int port,
+                                                bool ipv6,
+                                                bool ssl)
     {
-        self->init(server, port, ipv6, ssl);
+        return new WeeChatConnection(server, port, ipv6, ssl);
     }
 
-    char *WeeChatConnection_send_msg(WeeChatConnection* self, char *message)
+    void WeeChatConnection_send_msg(WeeChatConnection* self, char *message)
     {
-        std::string ret = self->send_msg(message);
-        char* array = new char[ret.size()+1];
-        strncpy(array, ret.c_str(), ret.size() + 1);
-        return array;
+        self->send_msg(message);
     }
 
     void free_buf(char* buf)
