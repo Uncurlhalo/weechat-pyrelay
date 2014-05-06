efd60f4c14988e88055525bcef9a305adbd51016
Jacob Melton <jmelton116@gmail.com>
2014-05-06 18:25:21 -0400

    Continued work on client.py, created framework for useful local objects

--- weechat_py/client.py
+++ weechat_py/client.py
@@ -1,6 +1,7 @@
-import relay
 import yaml
+import relay
 import argparse
+import weechatObjects as objs
 
 parser = argparse.ArgumentParser(description='Parse some args yo')
 parser.add_argument('--password', '-p', type=str)
@@ -15,12 +16,32 @@ ssl = config['SSL']
 myRelay = relay.WeechatRelay(host, port, IPv6, ssl)
 myRelay.init(args.password)
 
+# First get a list of buffers and create a buffer object for each
 myRelay.send('(listbuffers) hdata buffer:gui_buffers(*) number,full_name,short_name,type,nicklist,title,local_variables')
-buffer_list = myRelay.recieve()
-buffer_list = buffer_list.objects[0]
-myRelay.send('(listlines) hdata buffer:gui_buffers(*)/own_lines/last_line(-50)/data date,displayed,prefix,message')
-lines = myRelay.recieve()
-lines = lines.objects[0]
+reply = myRelay.recieve()
+buffers = {}
+bufferObject = reply.objects[0]
+bufferItemList = bufferObject.value['items']
+for item in bufferItemList:
+	name = item['full_name']
+	path = item['__path'][0]
+	buffers[name] = objs.WeechatBuffer(path, name)
+
+# For each buffer object get its last 100 lines
+# Make the buffers.lines value equal to the new list of lines
+for key,path in buffers.items():
+	lines = []
+	myRelay.send("(listlines) hdata buffer:{0}"
+				  "/own_lines/last_line(-50)/data "
+				  "date,displayed,prefix,message".format(path))
+	reply = myRelay.recieve()
+	linesObject = reply.objects[0]
+	linesItemList = linesObject.value['items']
+	for item in linesItemList:
+		lines.append(item['message'])
+
+	buffers[key].lines = lines
+
 myRelay.send('(nicklist) nicklist')
 nicklist = myRelay.recieve()
 nicklist =  nicklist.objects[0]
---
+++ weechat_py/weechatObjects.py
@@ -0,0 +1,11 @@
+class WeechatBuffer():
+	def __init__(self, path, name):
+		self.lines = []
+		self.path = path
+		self.name = name
+
+	def __str__(self):
+		# since I want to be able to utilize the weechat color codes
+		# have a __str__ function is useful since it will allow me to test 
+		# parsing the color codes and turning them into logical python
+		# for generating color.
